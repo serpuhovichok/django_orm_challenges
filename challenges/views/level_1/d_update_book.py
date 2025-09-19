df4cb5a79d01b82efadd3d8bd12bbe23bba0dc17
serpuhovichok <serpuhovichok@yandex.ru>
2025-09-19 18:20:43 +0300

    HW level_1

--- challenges/views/level_1/d_update_book.py
+++ challenges/views/level_1/d_update_book.py
@@ -13,8 +13,15 @@ from challenges.models import Book
 
 
 def update_book(book_id: int, new_title: str, new_author_full_name: str, new_isbn: str) -> Book | None:
-    # код писать тут
-    pass
+    try:
+        book = Book.objects.get(id=book_id)
+        book.title = new_title
+        book.author_full_name = new_author_full_name
+        book.isbn = new_isbn
+        book.save()
+        return book
+    except Book.DoesNotExist:
+        return None
 
 
 def update_book_handler(request: HttpRequest, book_id: int) -> HttpResponse:
