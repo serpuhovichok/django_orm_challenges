6d443feff6a7785b124d952ad04a05f582927d48
serpuhovichok <serpuhovichok@yandex.ru>
2025-10-08 20:38:20 +0300

    HW level_2

--- challenges/models.py
+++ challenges/models.py
@@ -8,3 +8,60 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Laptop(models.Model):
+    BRANDS = {
+        ("DELL", "DELL"),
+        ("Lenovo", "Lenovo"),
+        ("MacBook", "MacBook"),
+    }
+    brand = models.CharField(max_length=256, choices=BRANDS)
+    year = models.SmallIntegerField()
+    RAM = models.SmallIntegerField()
+    HDD = models.SmallIntegerField()
+    price = models.FloatField()
+    count_available = models.SmallIntegerField()
+    added_date = models.DateField()
+
+    def to_json(self):
+        return {
+            'brand': self.brand,
+            'year': self.year,
+            'RAM': self.RAM,
+            'HDD': self.HDD,
+            'price': self.price,
+            'count_available': self.count_available,
+            'added_date': self.added_date
+        }
+
+
+class BlogPost(models.Model):
+    STATUSES = {
+        ("published", "опубликован"),
+        ("not_published", "не опубликован"),
+        ("banned", "забанен"),
+    }
+    CATEGORIES = {
+        ('computers', 'Компьютеры'),
+        ('soft', 'Софт'),
+        ('coding', 'Кодинг'),
+    }
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    author = models.CharField(max_length=256)
+    status = models.CharField(max_length=20, choices=STATUSES)
+    create_date = models.DateField()
+    publish_date = models.DateField()
+    category = models.CharField(max_length=20, choices=CATEGORIES)
+
+    def to_json(self):
+        return {
+            'title': self.title,
+            'text': self.text,
+            'author': self.author,
+            'status': self.status,
+            'create_date': self.create_date,
+            'publish_date': self.publish_date,
+            'category': self.category
+        }
--- challenges/views/level_2/b_blog.py
+++ challenges/views/level_2/b_blog.py
@@ -9,14 +9,21 @@
 - реализовать у модели метод to_json, который будет преобразовывать объект книги в json-сериализуемый словарь
 - по очереди реализовать каждую из вьюх в этом файле, проверяя правильность их работу в браузере
 """
-from django.http import HttpRequest, HttpResponse
+import datetime
+
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from challenges.models import BlogPost
 
 
 def last_posts_list_view(request: HttpRequest) -> HttpResponse:
     """
     В этой вьюхе вам нужно вернуть 3 последних опубликованных поста.
     """
-    pass
+    posts = BlogPost.objects.order_by('-publish_date')[:3]
+    result = []
+    for post in posts:
+        result.append(post.to_json())
+    return JsonResponse(result, safe=False)
 
 
 def posts_search_view(request: HttpRequest) -> HttpResponse:
@@ -25,27 +32,49 @@ def posts_search_view(request: HttpRequest) -> HttpResponse:
     Сам запрос возьмите из get-параметра query.
     Подходящесть поста можете определять по вхождению запроса в название или текст поста, например.
     """
-    pass
+    query = request.GET['query']
+    result = []
+    for post in BlogPost.objects.filter(title__contains=query):
+        if post.to_json() not in result:
+            result.append(post.to_json())
+    for post in BlogPost.objects.filter(text__contains=query):
+        if post.to_json() not in result:
+            result.append(post.to_json())
+
+    return JsonResponse(list(result), safe=False)
 
 
 def untagged_posts_list_view(request: HttpRequest) -> HttpResponse:
     """
     В этой вьюхе вам нужно вернуть все посты без категории, отсортируйте их по автору и дате создания.
     """
-    pass
-
+    posts = BlogPost.objects.filter(category__isnull=True).order_by('author', 'create_date')
+    result = []
+    for post in posts:
+        result.append(post.to_json())
+    return JsonResponse(result, safe=False)
 
 def categories_posts_list_view(request: HttpRequest) -> HttpResponse:
     """
     В этой вьюхе вам нужно вернуть все посты все посты, категория которых принадлежит одной из указанных.
     Возьмите get-параметр categories, в нём разделённый запятой список выбранных категорий.
     """
-    pass
-
+    categories = request.GET['categories'].split(',')
+    posts = BlogPost.objects.filter(category__in=categories)
+    result = []
+    for post in posts:
+        result.append(post.to_json())
+    return JsonResponse(result, safe=False)
 
 def last_days_posts_list_view(request: HttpRequest) -> HttpResponse:
     """
     В этой вьюхе вам нужно вернуть посты, опубликованные за последние last_days дней.
     Значение last_days возьмите из соответствующего get-параметра.
     """
-    pass
+    last_days = int(request.GET['last_days'])
+    date_border = datetime.datetime.now().date() - datetime.timedelta(days=last_days)
+    posts = BlogPost.objects.filter(publish_date__gte=date_border)
+    result = []
+    for post in posts:
+        result.append(post.to_json())
+    return JsonResponse(result, safe=False)
